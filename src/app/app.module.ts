920970ffc253e8ab9d6c82e809246676348fd35f
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-03-30 13:26:30 -0500

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { View1Component } from './view1/view1.component';
+import { View2Component } from './view2/view2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    View1Component,
+    View2Component
   ],
   imports: [
     BrowserModule,
